d95169cc92669e8ccecdc277b64031f95e8d3e5c
root <root@localhost.localdomain>
2018-11-22 08:25:18 -0500

    first commit

---
+++ stdstring.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+
+class Stdstring
+{
+    char* a;
+    int capacity;
+    static int npos = -1;
+
+public:
+    Stdstring();
+    Stdstring(const Stdstring&);
+    Stdstring(const char*);
+    string& operator
+};
